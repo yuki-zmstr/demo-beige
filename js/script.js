dfcfa476bd541f424ee0105b78176324fd455c5c
Yukihide Takahashi <yukihide99@gmail.com>
2021-05-13 13:27:50 +0800

    added more stuff

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     var wh = $(window).height();
-    var ww = $(window).width();
 
     // freeze screen when logo is in play, then logo fade out
     $(function() {
@@ -21,21 +20,7 @@ $(document).ready(function() {
     $("header").delay(5000).css({ opacity: 0.0, visibility: "visible" }).animate({ opacity: 1.0 }, 1000);
     $(".selection").delay(5000).css({ opacity: 0.0, visibility: "visible" }).animate({ opacity: 1.0 }, 1000);
     $(".sidenav").delay(5000).css({ opacity: 0.0, visibility: "visible" }).animate({ left: 10, opacity: 1.0 }, 1000)
-        // $("#content").delay(4000).css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0},1000);
 
-    /* For the sticky navigation */
-    // var lastScrollTop = 0;
-    // $(window).scroll(function(event) {
-    //     var st = $(this).scrollTop();
-    //     if ($("#header").hasClass('up') || $("#header").hasClass('down')) {
-    //         if (st > lastScrollTop) {
-    //             $("#header").removeClass('up').animate({ opacity: 0.2 }, 500).addClass('down');
-    //         } else {
-    //             $("#header").removeClass('down').animate({ opacity: 1.0 }, 500).addClass('up')
-    //         }
-    //     }
-    //     lastScrollTop = st;
-    // });
 
     // show and hide the hidden nav bar
     function showhide() {
@@ -65,7 +50,6 @@ $(document).ready(function() {
             $(this).attr("disabled", true);
             var that = this;
             setTimeout(function() { $(that).removeAttr("disabled") }, 600);
-            // console.log('clicked');
 
         }
 
@@ -80,24 +64,7 @@ $(document).ready(function() {
     $("#btn_menu i").click(showhide)
     $(".selection li").click(showhide)
 
-
-    /* Navigation scroll */
-    // $(function() {
-    //     $('a[href*=#]:not([href=#])').click(function() {
-    //         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    //             var target = $(this.hash);
-    //             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    //             if (target.length) {
-    //                 $('html,body').animate({
-    //                     scrollTop: target.offset().top
-    //                 }, 1000);
-    //                 return false;
-    //             }
-    //         }
-    //     });
-    // });
-
-    // Hide Header on on scroll down
+    // Hide Header on scroll down
     var didScroll;
     var lastScrollTop = 0;
     var delta = 5;
@@ -117,12 +84,8 @@ $(document).ready(function() {
     function hasScrolled() {
         var st = $(this).scrollTop();
 
-        // Make sure they scroll more than delta
         if (Math.abs(lastScrollTop - st) <= delta)
             return;
-
-        // If they scrolled down and are past the navbar, add class .nav-up.
-        // This is necessary so you never see what is "behind" the navbar.
         if (st > lastScrollTop && st > navbarHeight) {
             // Scroll Down
             $('header').removeClass('nav-down').addClass('nav-up');
@@ -136,11 +99,8 @@ $(document).ready(function() {
     }
 
     // animate for each block
-    var start_pos = 0;
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
-        var footerPos = $("#footer").offset().top;
-        var contactPos = $("#contact").offset().top
         $(".sc").each(function() {
             var elemPos = $(this).offset().top;
             if (scroll > elemPos - wh / 1.3) {
@@ -159,46 +119,5 @@ $(document).ready(function() {
                 $(this).css({ opacity: 0.0, visibility: "visible" }).animate({ opacity: 1.0 }, 1000);
             }
         });
-
-
     });
-
 });
-
-
-
-
-// form validation
-
-// $(function() {
-//     $("#form").submit(function(event) {
-//         var name = $("#name").val();
-//         var message = $("#message").val();
-//         validateNameField(name, event);
-//         validateMessageField(message, event);
-//         if (confirm("Confirm submission")) {
-//             alert("Thank you for your message")
-//             return
-//         } else {
-//             event.preventDefault()
-//         }
-//     });
-
-//     function validateNameField(name, event) {
-//         if (name.trim().length < 3) {
-//             alert("Please enter a valid name");
-//             event.preventDefault();
-//         } else {
-//             return
-//         }
-//     };
-
-//     function validateMessageField(message, event) {
-//         if (message.trim() == "") {
-//             alert("Please enter a valid message");
-//             event.preventDefault();
-//         } else {
-//             return
-//         }
-//     }
-// });
